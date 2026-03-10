374105a27e2be7df9f1469e6b1b52dc075bc3a80
Vishnu033 <132803974+Vishnu033@users.noreply.github.com>
2026-03-10 22:48:37 +0530

    Added even or odd checker

---
+++ LargestofTWO.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a first number: "))
+b=int(input("Enter a second number: "))
+if a>b:
+    print("Largest number is: ",a)
+elif a<b:
+    print("Largest number is: ",b)
+else:
+    print("Both numbers are equal")
